291c8a0bc93d456f9fea0f6b56ab0b9fc53da0de
Wholesome Ghoul <wholesome.ghoul@gmail.com>
2022-08-15 00:25:42 +0400

    perf: minify css

--- scripts/scss-to-css.sh
+++ scripts/scss-to-css.sh
@@ -2,5 +2,5 @@
 
 # INTENDED TO RUN FROM PROJECT ROOT
 
-yarn node-sass src/ -o dist/ &&
+yarn node-sass src/ -o dist/ --output-style compressed &&
   grep -rlZ '\.scss' ./dist | xargs -0 -r sed -i 's/\.scss/\.css/g'
